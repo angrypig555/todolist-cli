c44c674c6bee9d40b125a12e3530ca247cf9d7db
angrypig555 <84266804+angrypig555@users.noreply.github.com>
2025-12-29 12:05:35 +0000

    add more commands

--- todolist-cli.cpp
+++ todolist-cli.cpp
@@ -1,10 +1,56 @@
 ﻿#include<iostream>
 #include<string>
 #include<cstdlib>
+#include<vector>
+
+class ExitCmd {
+	public:
+		void execute(int8_t os, int8_t &running) {
+			if (os == 1) {
+				system("cls");
+			}
+			else {
+				system("clear");
+			}
+			std::cout << "Exiting" << std::endl;
+			running = 0;
+		}
+};
+
+class HelpCmd {
+	public:
+		void execute() {
+			std::cout << "Commands:" << std::endl;
+			std::cout << "exit -- Exits the program" << std::endl;
+			std::cout << "help -- Displays this message" << std::endl;
+			std::cout << "clear -- Clears the screen except the title" << std::endl;
+			std::cout << "list -- Lists the current list" << std::endl;
+			std::cout << "add -- Add an item to the list" << std::endl;
+			std::cout << "remove -- Remove an item from the list";
+			std::cout << "done -- Marks the specified item as done on the list" << std::endl;
+		}
+};
+
+class ClearCmd {
+public:
+	void execute(int8_t os, std::string name) {
+		if (os == 1) {
+			system("cls");
+		}
+		else {
+			system("clear");
+		}
+		std::cout << "List: " << name << std::endl;
+	}
+};
+
 
 int main()	{
 	std::string name;
 	std::int8_t os; // 1 means windows, 0 means unix
+	std::int8_t running; // 1 means running, 0 means stopped
+
+	running = 1;
 	std::cout << "todolist-cli v1" << std::endl;
 	std::cout << "Please enter a name for this list:";
 	std::cin >> name;
@@ -24,9 +70,30 @@ int main()	{
 		os = 0;
 	}
 
-	std::cout << "List:" << name << std::endl;
-	while (true) {
-
+	std::cout << "List: " << name << std::endl;
+	ExitCmd exit;
+	ClearCmd clear;
+	HelpCmd help;
+	std::string cmd_input;
+	std::vector<std::string> list = {};
+	std::cout << "todolist-cli -- Type help for a list of commands." << std::endl;
+	while (running == 1) {
+		cmd_input = "";
+		std::cout << "todolist-cli> ";
+		std::cin >> cmd_input;
+		switch (cmd_input[0]) {
+			case 'e':
+				exit.execute(os, running);
+				break;
+			case 'h':
+				help.execute();
+				break;
+			case 'c':
+				clear.execute(os, name);
+				break;
+			default:
+				std::cout << "Unknown command, type help for a list of commands." << std::endl;
+		}
 	}
 	return 0;
 }
