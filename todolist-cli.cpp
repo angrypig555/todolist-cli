73c07bf816213075ed3107569d6b93155a96dae2
angrypig555 <84266804+angrypig555@users.noreply.github.com>
2025-12-29 10:45:39 +0000

    Add project files.

---
+++ todolist-cli.cpp
@@ -0,0 +1,12 @@
+﻿// todolist-cli.cpp : Defines the entry point for the application.
+//
+
+#include "todolist-cli.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
