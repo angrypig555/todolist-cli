0b4d2a21caa665bde476c897858ca25cba5c78b1
angrypig555 <84266804+angrypig555@users.noreply.github.com>
2025-12-29 11:05:06 +0000

    initial setup

--- todolist-cli.cpp
+++ todolist-cli.cpp
@@ -1,12 +1,32 @@
-﻿// todolist-cli.cpp : Defines the entry point for the application.
-//
+﻿#include<iostream>
+#include<string>
+#include<cstdlib>
 
-#include "todolist-cli.h"
+int main()	{
+	std::string name;
+	std::int8_t os; // 1 means windows, 0 means unix
+	std::cout << "todolist-cli v1" << std::endl;
+	std::cout << "Please enter a name for this list:";
+	std::cin >> name;
+	// clearing terminal and defining os
+	int unix = system("clear");
+	if (unix != 0) {
+		int windows = system("cls");
+		if (windows != 0) {
+			std::cerr << "Error clearing terminal, please switch to a system that supports the clear or the cls function." << std::endl;
+			return 1;
+		}
+		else {
+			os = 1;
+		}
+	}
+	else {
+		os = 0;
+	}
 
-using namespace std;
+	std::cout << "List:" << name << std::endl;
+	while (true) {
 
-int main()
-{
-	cout << "Hello CMake." << endl;
+	}
 	return 0;
 }
